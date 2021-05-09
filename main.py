d0b44d1fce5139eaac9ab8f903b63f844199b958
Emperor963 <83712724+Emperor963@users.noreply.github.com>
2021-05-09 22:58:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,18 @@ async def math(ctx, *, arg):
     x = numexpr.evaluate(arg)
     await ctx.send(x)
 
+#Edited to a better code for showing the axes with origin at (0,0)
 @bot.command()
 async def plot(ctx, *, eqn):
     fn = Expression(eqn, ['x'])
     if 'x' not in fn:  # check 'x' is used in eqn
         print("Error: Equation must have a variable named x")
+    ax = plt.axes()
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
     x = np.linspace(-10, 10, 100)
-    plt.clf()
     plt.plot(x, fn(x))
     plt.xlabel('x - axis')
     plt.ylabel('y - axis')
@@ -29,6 +34,8 @@ async def plot(ctx, *, eqn):
     plt.savefig(img)
     img.seek(0)
     await ctx.send(file=discord.File(img, "graph.png"))
+    plt.clf()
+    
     
 @bot.command()
 async def latexfind(ctx, *, eqn):
